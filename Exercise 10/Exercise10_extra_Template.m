5d8ea52ee6d45245c7bd175e0405ba81529191ae
Christian Segercrantz <segercc1@aalto.fi>
2021-12-07 13:38:37 +0200

    Exercise 10

---
+++ Exercise 10/Exercise10_extra_Template.m
@@ -0,0 +1,28 @@
+Price=[1 4 2;0.25 1 0.5;0.5 2 1]
+Features=[1 1/6 0.5;6 1 4;2 0.25 1]
+Memory=[1 1/9 0.5;9 1 4;2 0.25 1]
+Criteria=[1 3 5;1/3 1 2;0.2 0.5 1]
+
+%a) and b)
+%Calculation of local priority vectors and consistency indices
+
+%For each variable:
+
+%1. solve eigenvectors and eigenvalues:
+
+%2. Discover largest eigenvalue and the corresponding eigenvector:
+
+%3. calculate local priority vector:
+
+%4. calculate consistency index:
+
+%5. calculate consistency ratio:
+
+
+
+%c)
+%Calculation of total priorities
+
+%d) 
+%Repetition of calculations with replica of A added to the set of alternatives
+
